32c6c8ae9d3e6ed899ae197bb4b78e47f7851bcd
Wayne Small <waynemystir@gmail.com>
2015-04-05 14:51:39 -0400

    more parsing and reorganizing

--- ota-ios/ota-ios/Models/Ean/HotelListResponse/EanHotelListHotelSummary.h
+++ ota-ios/ota-ios/Models/Ean/HotelListResponse/EanHotelListHotelSummary.h
@@ -10,14 +10,34 @@
 
 @interface EanHotelListHotelSummary : NSObject
 
+@property (nonatomic) NSUInteger order;
 @property (nonatomic, strong) NSString *hotelId;
 @property (nonatomic, strong) NSString *hotelName;
-@property (nonatomic, strong) NSNumber *highRate;
+@property (nonatomic, strong) NSString *address1;
+@property (nonatomic, strong) NSString *city;
+@property (nonatomic, strong) NSString *stateProvinceCode;
+@property (nonatomic, strong) NSString *postalCode;
+@property (nonatomic, strong) NSString *countryCode;
+@property (nonatomic, strong) NSString *airportCode;
+@property (nonatomic, strong) NSString *supplierType;
+@property (nonatomic) id propertyCategory;
+@property (nonatomic, strong) NSNumber *hotelRating;
+@property (nonatomic, strong) NSNumber *confidenceRating;
+@property (nonatomic) id amenityMask;
 @property (nonatomic, strong) NSNumber *tripAdvisorRating;
-@property (nonatomic, strong) NSString *thumbNailUrl;
+@property (nonatomic, strong) NSString *locationDescription;
+@property (nonatomic, strong) NSString *shortDescription;
+@property (nonatomic, strong) NSNumber *highRate;
+@property (nonatomic, strong) NSNumber *lowRate;
+@property (nonatomic, strong) NSString *rateCurrencyCode;
 @property (nonatomic) double latitude;
 @property (nonatomic) double longitude;
-@property (nonatomic, strong) NSString *shortDescription;
+@property (nonatomic, strong) NSNumber *proximityDistance;
+@property (nonatomic, strong) NSString *proximityUnit;
+@property (nonatomic) id hotelInDestination;
+@property (nonatomic, strong) NSString *thumbNailUrl;
+@property (nonatomic, strong) NSString *deepLink;
+@property (nonatomic, strong) NSDictionary *roomRateDetailsList;
 
 + (EanHotelListHotelSummary *)hotelFromObject:(id)object;
 
--- ota-ios/ota-ios/Models/Ean/HotelListResponse/EanHotelListHotelSummary.m
+++ ota-ios/ota-ios/Models/Ean/HotelListResponse/EanHotelListHotelSummary.m
@@ -20,14 +20,35 @@
     //*******************
     // TODO: handle cases where the following values are not empty or wrong data type
     //*******************
+    hotel.order = [[object objectForKey:@"@order"] integerValue];
     hotel.hotelId = [object objectForKey:@"hotelId"];
     hotel.hotelName = [object objectForKey:@"name"];
-    hotel.highRate = [object objectForKey:@"highRate"];
+    hotel.address1 = [object objectForKey:@"address1"];
+    hotel.city = [object objectForKey:@"city"];
+    hotel.stateProvinceCode = [object objectForKey:@"stateProvinceCode"];
+    hotel.postalCode = [object objectForKey:@"postalCode"];
+    hotel.countryCode = [object objectForKey:@"countryCode"];
+    hotel.airportCode = [object objectForKey:@"airportCode"];
+    hotel.supplierType = [object objectForKey:@"supplierType"];
+    hotel.propertyCategory = [object objectForKey:@"propertyCategory"];
+    hotel.hotelRating = [object objectForKey:@"hotelRating"];
+    hotel.confidenceRating = [object objectForKey:@"confidenceRating"];
+    hotel.amenityMask = [object objectForKey:@"amenityMask"];
     hotel.tripAdvisorRating = [object objectForKey:@"tripAdvisorRating"];
-    hotel.thumbNailUrl = [object objectForKey:@"thumbNailUrl"];
+    hotel.locationDescription = [object objectForKey:@"locationDescription"];
+    hotel.shortDescription = [object objectForKey:@"shortDescription"];
+    hotel.highRate = [object objectForKey:@"highRate"];
+    hotel.lowRate = [object objectForKey:@"lowRate"];
+    hotel.rateCurrencyCode = [object objectForKey:@"rateCurrencyCode"];
     hotel.latitude = [[object objectForKey:@"latitude"] doubleValue];
     hotel.longitude = [[object objectForKey:@"longitude"] doubleValue];
-    hotel.shortDescription = [object objectForKey:@"shortDescription"];
+    hotel.proximityDistance = [object objectForKey:@"proximityDistance"];
+    hotel.proximityUnit = [object objectForKey:@"proximityUnit"];
+    hotel.hotelInDestination = [object objectForKey:@"hotelInDestination"];
+    hotel.thumbNailUrl = [object objectForKey:@"thumbNailUrl"];
+    hotel.deepLink = [object objectForKey:@"deepLink"];
+    hotel.roomRateDetailsList = [object objectForKey:@"RoomRateDetailsList"];
+    
     return hotel;
 }
 
