f83201e6f1e16da4d91fc4c3733200e9d5d1f3a5
Wayne Small <waynemystir@gmail.com>
2015-04-09 00:28:23 -0400

    building out SelectRoomViewController

--- ota-ios/ota-ios/Controllers/SelectRoomViewController.m
+++ ota-ios/ota-ios/Controllers/SelectRoomViewController.m
@@ -17,9 +17,18 @@
 
 @interface SelectRoomViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollViewOutlet;
 @property (weak, nonatomic) IBOutlet UITableView *roomsTableViewOutlet;
+@property (weak, nonatomic) IBOutlet UIView *inputBookOutlet;
+@property (weak, nonatomic) IBOutlet UIButton *bookButtonOutlet;
+
+
 @property (nonatomic, strong) EanHotelRoomAvailabilityResponse *eanHrar;
+@property (nonatomic, strong) EanAvailabilityHotelRoomResponse *selectedRoom;
 @property (nonatomic, strong) NSArray *tableData;
+@property (nonatomic, strong) NSIndexPath *expandedIndexPath;
+
+- (IBAction)justPushIt:(id)sender;
 
 @end
 
@@ -35,7 +44,12 @@
     
     self.roomsTableViewOutlet.dataSource = self;
     self.roomsTableViewOutlet.delegate = self;
+    
+    self.inputBookOutlet.hidden = YES;
+    self.inputBookOutlet.frame = CGRectMake(10.0f, 412.0f, 300.0f, 0.0f);
 }
+
+#pragma mark LoadDataProtocol methods
 
 - (void)requestFinished:(NSData *)responseData {
     self.eanHrar = [EanHotelRoomAvailabilityResponse roomsAvailableResponseFromData:responseData];
@@ -62,6 +76,11 @@
         cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     }
     
+    // Set these so that the cell expansion and compression work well
+    // Curtesy of http://stackoverflow.com/questions/10220565/expanding-uitableviewcell
+    cell.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    cell.clipsToBounds = YES;
+    
     EanAvailabilityHotelRoomResponse *room = [EanAvailabilityHotelRoomResponse roomFromDict:[self.tableData objectAtIndex:indexPath.row]];
     
     cell.roomTypeDescriptionOutlet.text = room.roomTypeDescription;
@@ -69,15 +88,120 @@
     return cell;
 }
 
+#pragma mark UITableViewDelegate methods
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    SelectionCriteria *sc = [SelectionCriteria singleton];
-    EanAvailabilityHotelRoomResponse *room = [EanAvailabilityHotelRoomResponse roomFromDict:[self.tableData objectAtIndex:indexPath.row]];
+    //    [self bookIt];
     
+    // If the index path of the currently expanded cell is the same as the index that
+    // has just been tapped, then set the expanded index to nil so that there aren't any
+    // expanded cells, otherwise, set the expanded index to the index that has just
+    // been selected.
+    //
+    // Curtesy of 0x7fffffff from http://stackoverflow.com/questions/4635338/uitableviewcell-expand-on-click
+    
+    if ([indexPath compare:self.expandedIndexPath] == NSOrderedSame) {
+        self.expandedIndexPath = nil;
+        [self compressFromDetailViews];
+    } else {
+        self.expandedIndexPath = indexPath;
+        [self expandToDetailViews];
+    }
+    
+    [tableView beginUpdates]; // tell the table you're about to start making changes
+    [tableView endUpdates]; // tell the table you're done making your changes
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    // If the expandedIndexPath is not nil and equals this indexPath, then return
+    // the height of the tableView
+    //
+    // Curtesy of 0x7fffffff from http://stackoverflow.com/questions/4635338/uitableviewcell-expand-on-click
+    if ([indexPath compare:self.expandedIndexPath] == NSOrderedSame) {
+        return tableView.frame.size.height;
+    }
+    
+    // Otherwise return zero if there is a selected row or the normal height if
+    // there is no selected row
+    if (nil != self.expandedIndexPath) {
+        return 0.0f;
+    } else {
+        return 44.0f;
+    }
+}
+
+#pragma mark Various methods
+
+- (void)expandToDetailViews {
+    self.scrollViewOutlet.contentSize = CGSizeMake(320.0f, 800.0f);
+    
+    __weak UIView *ibo = self.inputBookOutlet;
+    ibo.hidden = NO;
+    [UIView animateWithDuration:0.3 animations:^{
+        ibo.frame = CGRectMake(10, 412, 300, 300);
+    } completion:^(BOOL finished) {
+        ;
+    }];
+}
+
+- (void)compressFromDetailViews {
+    self.scrollViewOutlet.contentSize = CGSizeMake(320.0f, 508.0f);
+    
+    __weak UIView *ibo = self.inputBookOutlet;
+    [UIView animateWithDuration:0.3 animations:^{
+        ibo.frame = CGRectMake(10, 412, 300, 0);
+    } completion:^(BOOL finished) {
+        ibo.hidden = YES;
+    }];
+}
+
+- (void)bookIt {
+    if (nil == self.expandedIndexPath) {
+        return;
+    }
+    
+    self.selectedRoom = [EanAvailabilityHotelRoomResponse roomFromDict:[self.tableData objectAtIndex:self.expandedIndexPath.row]];
+    SelectionCriteria *sc = [SelectionCriteria singleton];
     BookViewController *bvc = [BookViewController new];
     
-    [[LoadEanData sharedInstance:bvc] bookHotelRoomWithHotelId:self.eanHrar.hotelId arrivalDate:sc.arrivalDateEanString departureDate:sc.returnDateEanString supplierType:room.supplierType rateKey:self.eanHrar.rateKey roomTypeCode:room.roomTypeCode rateCode:room.rateCode chargeableRate:room.chargeableRate numberOfAdults:sc.numberOfAdults childTravelers:[ChildTraveler childTravelers] room1FirstName:@"test" room1LastName:@"testers" room1BedTypeId:@"23" room1SmokingPreference:@"NS" affiliateConfirmationId:[NSUUID UUID] email:@"test@yourSite.com" firstName:@"tester" lastName:@"testing" homePhone:@"1234567890" creditCardType:@"CA" creditCardNumber:@"5401999999999999" creditCardIdentifier:@"123" creditCardExpirationMonth:@"11" creditCardExpirationYear:@"2016" address1:@"travelnow" city:@"Bellevue" stateProvinceCode:@"WA" countryCode:@"US" postalCode:@"98004"];
+    [[LoadEanData sharedInstance:bvc] bookHotelRoomWithHotelId:self.eanHrar.hotelId
+                                                   arrivalDate:sc.arrivalDateEanString
+                                                 departureDate:sc.returnDateEanString
+                                                  supplierType:self.selectedRoom.supplierType
+                                                       rateKey:self.eanHrar.rateKey
+                                                  roomTypeCode:self.selectedRoom.roomTypeCode
+                                                      rateCode:self.selectedRoom.rateCode
+                                                chargeableRate:self.selectedRoom.chargeableRate
+                                                numberOfAdults:sc.numberOfAdults
+                                                childTravelers:[ChildTraveler childTravelers]
+                                                room1FirstName:@"test"
+                                                 room1LastName:@"testers"
+                                                room1BedTypeId:@"23"
+                                        room1SmokingPreference:@"NS"
+                                       affiliateConfirmationId:[NSUUID UUID]
+                                                         email:@"test@yourSite.com"
+                                                     firstName:@"tester"
+                                                      lastName:@"testing"
+                                                     homePhone:@"1234567890"
+                                                creditCardType:@"CA"
+                                              creditCardNumber:@"5401999999999999"
+                                          creditCardIdentifier:@"123"
+                                     creditCardExpirationMonth:@"11"
+                                      creditCardExpirationYear:@"2016"
+                                                      address1:@"travelnow"
+                                                          city:@"Bellevue"
+                                             stateProvinceCode:@"WA"
+                                                   countryCode:@"US"
+                                                    postalCode:@"98004"];
     
     [self.navigationController pushViewController:bvc animated:YES];
+}
+
+- (IBAction)justPushIt:(id)sender {
+    if (sender == self.bookButtonOutlet) {
+        [self bookIt];
+    }
 }
 
 @end
